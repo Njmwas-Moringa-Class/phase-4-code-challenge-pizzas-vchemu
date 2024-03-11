145e7e60ba3b7009fbe9c3de5f72223190f72b6c
vchemu <valerine.chemutai@student.moringaschool.com>
2024-03-11 22:20:57 +0300

    first

--- server/models.py
+++ server/models.py
@@ -14,9 +14,14 @@ db = SQLAlchemy(metadata=metadata)
 class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
 
+    serialize_rules = ('-restaurant_pizzas.restaurant')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship(
+        'RestaurantPizza', backref='restaurant')
 
     # add relationship
 
@@ -29,9 +34,15 @@ class Restaurant(db.Model, SerializerMixin):
 class Pizza(db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
 
+    serialize_rules = ('-restaurant_pizzas.pizza')
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
+
+    restaurant_pizzas = db.relationship(
+        'RestaurantPizza', backref='pizza')
+    
 
     # add relationship
 
@@ -44,8 +55,13 @@ class Pizza(db.Model, SerializerMixin):
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = 'restaurant_pizzas'
 
+    serialize_rules = ('-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas')
+
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
     # add relationships
 
